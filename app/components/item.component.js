8cfcc1548e85a168424e86907dac758410371ffb
Yury Tatarynovich <Yury Tatarynovich>
2017-04-17 15:00:45 +0300

    new items

---
+++ app/components/item.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Item extends React.Component{
+  render() {
+    return (
+      <div className="item">
+        My TODO
+      </div>
+    );
+  }
+}
+
+export default Item
